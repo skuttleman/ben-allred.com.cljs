d35b161e430353ac1e2326e600637ead12300b58
Ben Allred <skuttleman@gmail.com>
2017-09-09 13:37:20 -0600

    Add font awesome

--- src/cljs/com/ben_allred/empire/reducers/d.cljs
+++
@@ -1,8 +0,0 @@
-(ns com.ben-allred.empire.reducers.d)
-
-(defn d
-    ([] 0)
-    ([state {type :type}]
-        (case type
-            :dec (dec state)
-            state)))
--- src/cljs/com/ben_allred/empire/reducers/i.cljs
+++
@@ -1,8 +0,0 @@
-(ns com.ben-allred.empire.reducers.i)
-
-(defn i
-    ([] 0)
-    ([state {type :type}]
-     (case type
-         :inc (inc state)
-         state)))
