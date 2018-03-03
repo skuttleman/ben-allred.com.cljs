6abbeb9148a99be12fd240292b97f882780fda34
Ben Allred <skuttleman@gmail.com>
2018-03-03 14:05:00 -0700

    Update bio and portfolio data

--- project.clj
+++ project.clj
@@ -45,7 +45,7 @@
                  ;; production. You can build this with:
                  ;; lein cljsbuild once min
                  {:id           "min"
-                  :source-paths ["src"]
+                  :source-paths ["src/cljs"]
                   :compiler     {:output-to     "resources/public/js/compiled/com/ben_allred.js"
                                  :main          com.ben-allred.core
                                  :optimizations :advanced
